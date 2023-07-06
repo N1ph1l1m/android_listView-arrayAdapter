4c8885c299535dd35a5bbe58362e7cd4563b997d
N1ph1l1m <zerorad@inbox.ru>
2023-07-06 14:55:58 +0300

    Create menu

---
+++ app/src/main/java/com/example/listviewarrayadapter/ListView/ListView3.java
@@ -0,0 +1,16 @@
+package com.example.listviewarrayadapter.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.listviewarrayadapter.R;
+
+public class ListView3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view3);
+    }
+}
