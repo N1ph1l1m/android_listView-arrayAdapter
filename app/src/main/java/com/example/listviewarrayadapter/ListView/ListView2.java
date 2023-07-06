b2c724a5b045daa918666a00a673d75028dcb67a
N1ph1l1m <zerorad@inbox.ru>
2023-07-06 15:34:02 +0300

    Create ArrayAdapter in listView2

--- app/src/main/java/com/example/listviewarrayadapter/ListView/ListView2.java
+++ app/src/main/java/com/example/listviewarrayadapter/ListView/ListView2.java
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.listviewarrayadapter.MainActivity;
 import com.example.listviewarrayadapter.R;
 
 public class ListView2 extends AppCompatActivity {
 
+    ListView  listView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view2);
+//
+//        listView2 = findViewById(R.id.listView2);
+//
+//        // получаем ресурс
+//        String[]Names = getResources().getStringArray(R.array.Names);
+//
+//        // создаем адаптер
+//        ArrayAdapter <String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,Names);
+//
+//        // устанавливаем для списка адаптер
+//        listView2.setAdapter(adapter);
     }
 
 
