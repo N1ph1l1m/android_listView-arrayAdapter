fb2196ffe321f874974a5cab9dd4b5f4823d8c8e
N1ph1l1m <zerorad@inbox.ru>
2023-07-10 10:18:34 +0300

    Update Spinner

--- app/src/main/java/com/example/listviewarrayadapter/SpinnerActivity.java
+++ app/src/main/java/com/example/listviewarrayadapter/SpinnerActivity.java
@@ -3,11 +3,15 @@ package com.example.listviewarrayadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SpinnerActivity extends AppCompatActivity {
 
+    TextView textView;
     Spinner spin;
     String[] color = {"Black","White","Green","Yellow","Blue"};
     @Override
@@ -15,11 +19,30 @@ public class SpinnerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner);
 
+        textView = findViewById(R.id.selection);
         spin = findViewById(R.id.spinnerView);
+
+
+
         // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,color);
         // Определяем разметку для использования при выборе элемента
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin.setAdapter(adapter);
+
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                String item = (String)adapterView.getItemAtPosition(position);
+                textView.setText(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        };
+        spin.setOnItemSelectedListener(itemSelectedListener);
     }
 }
